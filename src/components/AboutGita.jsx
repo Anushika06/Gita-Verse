fc9aef1060e2305da2361e2ca1a7ed00cfce068d
Anushika06 <anushika.24bcs10344@sst.scaler.com>
2025-04-21 20:05:51 +0530

    Update AboutGita.jsx

--- src/components/AboutGita.jsx
+++ src/components/AboutGita.jsx
@@ -11,7 +11,7 @@ function AboutGita() {
         />
       </div>
 
-      <div className="text-white text-xl lg:max-w-[70%] mt-50">
+      <div className="text-white text-xl lg:max-w-[70%] mt-30">
         <h2 className="font-semibold text-4xl mb-6 text-amber-300">About the Bhagavad Gita</h2>
         <p className="leading-relaxed italic">
           The Bhagavad Gita, or “Song of God,” is a profound dialogue between Lord Krishna and the warrior Arjuna, taking place on the battlefield of Kurukshetra. As Arjuna hesitates to fight against his own relatives, Krishna guides him through spiritual wisdom, addressing his moral confusion and fear. The Gita explores the eternal soul (atman), the importance of selfless duty (karma yoga), devotion (bhakti), and knowledge (jnana) as means to liberation. Krishna reveals His universal cosmic form and emphasizes detachment from the fruits of actions. He explains the three modes of material nature — goodness, passion, and ignorance — and highlights the value of surrendering to God. Ultimately, the Gita presents a timeless message: live righteously, act without selfish desire, and realize one’s true nature through love and devotion to the Divine.
